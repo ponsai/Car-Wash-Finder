1c106ae361c8a56ebd38d552f6e2307d33e9721e
VHB1609 <144184772+VHB1609@users.noreply.github.com>
2023-11-26 19:17:03 -0500

    change branch

--- react_app/carwash/src/components/about/Carousel.js
+++ react_app/carwash/src/components/about/Carousel.js
@@ -1,38 +1,40 @@
-import React, {useState} from "react"; 
+import React, {useState, useEffect} from "react"; 
 import Carousel from 'react-bootstrap/Carousel';
-
-const data = [
-  {
-   image: require('./images/aboutImage1.png'), 
-   caption:"Caption",
-   description:"Description Here"
-  },
-  {
-    image:require('./images/aboutImage2.png'), 
-    caption:"Caption",
-    description:"Description Here"
-   },
-   {
-    image:require('./images/aboutImage3.png'), 
-    caption:"Caption",
-    description:"Description Here"
-   } 
-]
 
 function AboutCarousel() {
   const [index, setIndex] = useState(0);
+  const [imagesData, setImagesData] = useState(null);
+
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
+  useEffect(async () => {
+    try {
+      if(!imagesData){
+        let url = 'http://localhost:5000/about-us';
+        const response = await fetch(url);
+        const about_us_data = await response.json();
+        setImagesData(about_us_data);
+        console.log(about_us_data);
+      }
+
+  } catch (error) {
+    console.log(error);
+  }
+  })
+
+
+
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-       {data.map((slide, i) => {
+       {imagesData && imagesData.map((slide, i) => {
+        const imageUrl = `data:image/png;base64,${slide.base64Image}`;
         return (
           <Carousel.Item>        
         <img
           className="d-block w-100"
-          src={slide.image}
+          src={imageUrl}
           alt="slider image"
         />
         <Carousel.Caption>
