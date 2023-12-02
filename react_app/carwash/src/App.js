2c6ba1f6ad0f12afb19afb5de156aa27cb0f0503
acreddy <acreddy97@outlook.com>
2023-12-01 21:25:25 -0800

    moved to a common folder

--- react_app/carwash/src/App.js
+++ react_app/carwash/src/App.js
@@ -1,23 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import Navigationbar from './components/Navigationbar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from './components/Home';
-import Details from './components/Details';
-import Locations from './components/Locations';
-import About from './components/About';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Navigationbar/>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/Details" component={Details} />
-            <Route exact path="/Locations" component={Locations} />
-            <Route exact path="/About" component={About} />
-          </Switch>
-          </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- react_app/carwash/src/index.js
+++ react_app/carwash/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from 'react-dom/client';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,7 +15,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 reportWebVitals();
+
