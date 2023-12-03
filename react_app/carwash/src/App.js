4c262d81a58139df3ec77cb8448cb2b7241d5568
ponsai <134491942+ponsai@users.noreply.github.com>
2023-12-02 23:40:43 -0800

    Update App.js
    Edited App.js so it didn't delete Arjuna's stuff.

--- react_app/carwash/src/App.js
+++ react_app/carwash/src/App.js
@@ -1,27 +1,48 @@
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import NavigationBar from './components/NavigationBar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Details from './components/Details';
 import Locations from './components/Locations';
 import About from './components/About';
 import Login from './components/Login';
+import ReviewForm from './components/ReviewForm';
+import CarWashForm from './components/CarWashForm';
+import CheckoutProcess from './components/CheckoutProcess';
+import carWashData from './data/carWashData';
+import Header from "./components/review/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Footer from './components/Footer';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const handleCartUpdate = (newCart) => {
+    setCart(newCart);
+  };
+
+  const handleFormSubmit = () => {
+    setCart([]); // Reset the cart after submission
+  };
+
   return (
     <div className="App">
-        <Router>
-          <NavigationBar/>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <Route exact path="/Details" component={Details} />
-            <Route exact path="/Locations" component={Locations} />
-            <Route exact path="/About" component={About} />
-          </Switch>
-          <Footer/>
-          </Router>
-    </div>
+      <Router>
+        <Header/>
+        <NavigationBar cartCount={cart.length}/> {/* Pass the cart length to the NavigationBar */}
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/Details" element={<Details />} />
+            <Route path="/wash-finder" element={<CarWashForm onCartUpdate={handleCartUpdate} />} />
+            <Route path="/About" element={<About />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/ReviewForm" element={<ReviewForm />} />
+            <Route path="/cart" element={<CheckoutProcess selectedServices={cart} carWashData={carWashData} onFormSubmit={handleFormSubmit} />} />
+          </Routes>
+        </div>
+        <Footer/>
   );
 }
 
