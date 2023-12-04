bdc064175b56221ac443059ab06020825f67ad5b
ponsai <tcglao@gmail.com>
2023-12-04 11:20:43 -0800

    Changed token to true in PrivateRoutes

--- react_app/carwash/src/components/PrivateRoute.js
+++ react_app/carwash/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoutes = () => {
-    let auth = {'token':true}
+    let auth = {'token':false}
     return (
         auth.token ? <Outlet/> : <Navigate to='/login'/>
     )
