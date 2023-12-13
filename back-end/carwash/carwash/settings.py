02361682358a7efe834e2344e66238196183ecea
ponsai <tcglao@gmail.com>
2023-12-12 20:40:04 -0800

    Updated readme

--- back-end/carwash/carwash/settings.py
+++ back-end/carwash/carwash/settings.py
@@ -137,11 +137,12 @@ DATABASES = {
     }
 }
 
+#bgzefgjqtntbooyf#
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'testuserdb1991@gmail.com'
-EMAIL_HOST_PASSWORD = 'mldzfiueiblagxlz'
+EMAIL_HOST_USER = '[YOUR EMAIL]'
+EMAIL_HOST_PASSWORD = '[APP PASSWORD FROM GMAIL]'
 EMAIL_USE_TLS = True
 
 # Password validation
